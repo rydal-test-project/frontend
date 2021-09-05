63b0edf2c66d071990b3e4a29fb9a9325c0b166a
rydal <i.am.alex2k@gmail.com>
2021-09-05 11:47:38 +0300

    Ui - input: added mask and ref

--- src/components/ui/input/index.tsx
+++ src/components/ui/input/index.tsx
@@ -1,8 +1,6 @@
 import React, {
-    ForwardedRef,
-    forwardRef,
-    memo,
-    ReactNode, SyntheticEvent, useRef, useState, Validator
+    memo, MouseEvent,
+    ReactNode, SyntheticEvent, useEffect, useState
 } from "react";
 import {IComponentProps} from "../../../common/types";
 import style from './style.module.scss'
@@ -13,37 +11,50 @@ import cn from 'classnames';
 interface IInputProps extends IComponentProps {
     mask?: string;
     placeholder?: ReactNode;
-    inputRef?: (el: HTMLInputElement) => void
+    inputRef?: (el: HTMLInputElement) => void;
+    onChange?: (value: string) => void;
 }
 
 const Input: React.NamedExoticComponent<IInputProps> = memo(({
                                                                  mask,
                                                                  placeholder,
+                                                                 onChange,
                                                                  inputRef: inputRefSetter
 }: IInputProps) => {
     const [isFocused, setIsFocused] = useState(false)
     const [value, setValue] = useState('')
-    let inputRef = null as null | HTMLInputElement
+    const [inputRef, setInputRef] = useState<HTMLInputElement | null>(null)
+
     const handleBlur = () => {
         setIsFocused(false)
     }
-    const handleClick = () => {
+    const handleSetInputRef = (el: HTMLInputElement) => {
+        setInputRef(el)
+        inputRefSetter && inputRefSetter(el)
+    }
+    const handleClick = (e: MouseEvent<HTMLElement>) => {
         setIsFocused(true)
         inputRef?.focus()
-    }
-    const setInputRef= (el: HTMLInputElement) => {
-        inputRef = el;
-        if (inputRefSetter) {
-            inputRefSetter(el)
-        }
+        e.stopPropagation()
     }
     const handleChange = (e: SyntheticEvent<HTMLInputElement>) => {
-        setValue(e.currentTarget.value)
+        const newValue = e.currentTarget.value
+
+        setValue(newValue)
+        onChange && onChange(newValue)
     }
+
+    useEffect(() => {
+        addEventListener('click',  handleBlur)
+
+        return () => {
+            removeEventListener('click',  handleBlur)
+        }
+    }, [])
 
     return (
         <div className={style.input} onClick={handleClick}>
-           <div>
+           <div className={cn(style.input__container)}>
                { placeholder && (
                    <div className={cn(style.input__placeholder, { [style.input__placeholder_toTop]: isFocused || value })}>
                        {placeholder}
@@ -51,7 +62,7 @@ const Input: React.NamedExoticComponent<IInputProps> = memo(({
                )}
 
                <InputMask
-                   mask={mask || ''} inputRef={setInputRef} onBlur={handleBlur} onChange={handleChange} />
+                   mask={mask || ''} inputRef={handleSetInputRef} onBlur={handleBlur} onChange={handleChange} />
            </div>
             <div className={cn(style.input__underline, { [style.input__underline__active]: isFocused || value })} />
         </div>
--- src/components/widgets/forms/Login.tsx
+++ src/components/widgets/forms/Login.tsx
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from "react";
+import React, {useState} from "react";
 import FormGroup from "../../ui/FormGroup";
 import {useInstance} from "react-ioc";
 import AuthService from "../../../services/auth";
@@ -18,22 +18,16 @@ export default function Login() {
       setPassword('');
     });
   };
-  const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    switch (event.target.name) {
-      case 'email': { setEmail(event.target.value); break }
-      case 'password': { setPassword(event.target.value); break }
-    }
-  };
 
   return (
     <section className="login">
       <h1 className="login__title">Кто вы?</h1>
       <form className="login__form form">
-        <FormGroup  labelFor="email">
-          <Input placeholder={'email'} />
+        <FormGroup >
+          <Input placeholder="Email" onChange={setEmail} />
         </FormGroup>
-        <FormGroup label="Пароль" labelFor="password">
-          <input className="form__control" value={password} type="password" name="password" id="password" onChange={event => inputHandler(event)} autoComplete="off"/>
+        <FormGroup  >
+          <Input placeholder="Пароль" onChange={setPassword} />
         </FormGroup>
         <div className="login__buttons">
           <button className="button button_primary" onClick={event => submitHandler(event)}>Попытаться войти</button>
